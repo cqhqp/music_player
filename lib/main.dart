3c8e9e33f20a9a69a6404d2e24aaeec30f1b8f05
SouPuppy <wdk1745@gmail.com>
2024-03-22 00:41:30 +0800

    UI Scaffold complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Menu Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: AudioPlayerScreen(),
     );
   }
 }
 
 class AudioPlayerScreen extends StatefulWidget {
-  const AudioPlayerScreen({Key? key}) : super(key: key);
-
   @override
   _AudioPlayerScreenState createState() => _AudioPlayerScreenState();
 }
@@ -26,274 +25,205 @@ class AudioPlayerScreen extends StatefulWidget {
 class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
   bool _isPlaying = false;
   int _currentIndex = 0; // 当前播放歌曲的索引
-  double _volume = 0.0;
+  double _volume = 0.5;
   double _progress = 0.0;
-  final List<String> _songs =  [
-    '周杰伦-漂移1.mp3',
-    '周杰伦-漂移2.mp3',
-    '周杰伦-漂移3.mp3',
-    '周杰伦-漂移4.mp3',
-    '周杰伦-漂移5.mp3',
-    '周杰伦-漂移6.mp3',
-    '周杰伦-漂移7.mp3',
-    '周杰伦-漂移8.mp3',
-    '周杰伦-漂移9.mp3',
-    '周杰伦-漂移10.mp3',
-    '周杰伦-漂移11.mp3',
-    '周杰伦-漂移12.mp3',
-    '周杰伦-漂移13.mp3',
-    '周杰伦-漂移14.mp3',
-    '周杰伦-漂移15.mp3',
-    '周杰伦-漂移16.mp3',
-    '周杰伦-漂移17.mp3',
-    '周杰伦-漂移18.mp3',
-    '周杰伦-漂移19.mp3',
-    '周杰伦-漂移20.mp3',
-    '周杰伦-漂移21.mp3',
-    '周杰伦-漂移22.mp3',
-    '周杰伦-漂移23.mp3',
-    '周杰伦-漂移24.mp3',
-    '周杰伦-漂移25.mp3',
-    '周杰伦-漂移26.mp3',
-    '周杰伦-漂移27.mp3',
-    '周杰伦-漂移28.mp3',
-    '周杰伦-漂移29.mp3',
-    '周杰伦-漂移30.mp3',
-  ]; 
+  final List<String> menu = [
+    'tabs1',
+    'tabs2',
+    'tabs3',
+  ]; // 假设音频文件名为 SongX.mp3
+  List<String> playList = [
+    'song1',
+    'song2',
+    'song3',
+  ];
 
-  Map<String, dynamic> arguments = {  
-    'key1': 'value1',  
-    'key2': 123,  
-    'key3': true,  
-  };  
-  static const MethodChannel methodChannel = MethodChannel('flutter_windows_cpp_plugin'); // 通道名
-
-  Future<void> _StopMethod() async {
-    try {
-      final bool result = await methodChannel.invokeMethod("yourMethodName",arguments); // 方法名
-      print("call _StopMethod result=$result.");
-    } on PlatformException catch (e) {
-      print("call _StopMethod Error!");
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _isPlaying = false;
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  void _playPause() async {
-    setState(() {
-      _isPlaying = !_isPlaying;
-    });
-  }
-
-  void _stop() async {
-    setState(() {
-      _StopMethod();
-    });
-  }
-
-  Widget _buildInfo() {
-    return const Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      // Image.asset('images/hello.jpeg', width: 200, height: 200),
-      Text(
-        // 歌曲名
-        '漂移',
-        style: TextStyle(fontSize: 50),
-      ),
-
-      SizedBox(height: 10),
-      // 其他歌曲信息（如艺术家、专辑等）
-      Text(
-        // 歌曲名
-        '周杰伦：头文字D',
-        style: TextStyle(fontSize: 20),
-      ),
-
-      SizedBox(height: 10),
-    ]);
-  }
-
-  Widget _buildPlayback() {
-    // 播放控制
-    return Row(children: <Widget>[
-      // 左边的控件
-      Container(
-        // color: Colors.blue,
-        child: Row(
-          children: [
-            IconButton(
-              icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-              iconSize: 50,
-              onPressed: _playPause,
-              tooltip: _isPlaying ? 'Pause' : 'Play',
+  void _showPlayList(BuildContext context) {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+      barrierColor: Colors.transparent, // 设置为透明
+      transitionDuration: const Duration(milliseconds: 200),
+      pageBuilder: (
+        BuildContext buildContext,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+      ) {
+        return Align(
+          alignment: Alignment.centerRight, // 从右侧弹出
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.3, // 设置宽度为屏幕宽度的30%
+            height: MediaQuery.of(context).size.height, // 菜单的高度与屏幕高度一致
+            color: Colors.white,
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text('Play List'),
+                leading: Container(), // 清空leading
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.close), // 使用close图标
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+              body: ListView.builder(
+                itemCount: playList.length, // 使用播放列表的长度
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(playList[index]), // 使用播放列表中的歌曲名
+                    onTap: () {
+                      _handleListItemTap(context, index);
+                    },
+                  );
+                },
+              ),
             ),
-            IconButton(
-              icon: const Icon(Icons.stop),
-              iconSize: 50,
-              onPressed: _stop,
-              tooltip: 'Stop',
-            ),
-            IconButton(
-              icon: const Icon(Icons.skip_next),
-              iconSize: 50,
-              onPressed: () {},
-              tooltip: 'Next Song',
-            ),
-            IconButton(
-              icon: const Icon(Icons.skip_previous),
-              iconSize: 50,
-              onPressed: () {},
-              tooltip: 'Previous Song',
-            ),
-          ],
-        ),
-      ),
-
-      // 占据剩余空间的Expanded widget
-      Expanded(
-        child: Container(
-          color: Colors.transparent, // 透明背景，仅用于占据空间
-        ),
-      ),
-
-      // 右边的控件
-      Container(
-        // width: 50, // 或者其他固定宽度
-        // height: 50,
-        // color: Colors.red,
-        child: Row(children: [
-          IconButton(
-            icon: const Icon(Icons.music_video),
-            iconSize: 50,
-            onPressed: () {},
-            tooltip: 'open music',
           ),
-          IconButton(
-            icon: const Icon(Icons.volume_up),
-            iconSize: 50,
-            onPressed: () {},
-            tooltip: 'Previous Song',
-          ),
-          Container(
-            width: 200.0, // 设置Container的宽度，Slider会继承这个宽度
-            child: Slider(
-              value: _volume,
-              min: 0.0,
-              max: 1.0,
-              onChanged: (double newValue) {
-                setState(() {
-                  _volume = newValue;
-                });
-              },
-            ),
-          )
-        ]),
-      ),
-    ]);
-  }
-
-  Widget _buildPlaylistItem(String song, int index) {
-    return ListTile(
-      title: Text(song),
-      leading: Radio(
-        value: index,
-        groupValue: _currentIndex,
-        onChanged: (int? value) {
-          if (value != null) {
-            setState(() {
-              _currentIndex = value;
-              _playPause();
-            });
-          }
-        },
-        activeColor: Theme.of(context).primaryColor,
-      ),
-      onTap: () {
-        setState(() {
-          _currentIndex = index;
-          _playPause();
-        });
+        );
       },
     );
   }
 
-  Widget _buildPlaylist() {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: _songs.length,
-      itemBuilder: (BuildContext context, int index) {
-        return _buildPlaylistItem(
-            _songs[index].substring(0, _songs[index].lastIndexOf('.')), index);
-      },
-    );
+  void _handleListItemTap(BuildContext context, int index) {
+    setState(() {
+      _currentIndex = index;
+      // 这里可以添加播放选中歌曲的逻辑
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Audio Player'),
-      // ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              flex: 7, // 左侧70%
-
+      body: Row(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.25, // 设置宽度为屏幕宽度的25%
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: menu.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(menu[index]),
+                        onTap: () {
+                          // Handle menu item tap
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.grey[200], // Placeholder color
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // 占据剩余空间的Expanded widget
+                  TopMenuBar(
+                    onPlaylistPressed: () {
+                      _showPlayList(context);
+                    },
+                  ),
                   Expanded(
-                    child: Container(
-                      color: Colors.transparent,
-                    ),
+                    child: Container(),
                   ),
-                  
-                  _buildInfo(),
-
-                  // 占据剩余空间的Expanded widget
-                  Expanded(
-                    child: Container(
-                      color: Colors.transparent,
-                    ),
-                  ),
-                  Container(
-                    child: Slider(
-                      value: _progress,
-                      min: 0.0,
-                      max: 100.0,
-                      onChanged: (double newValue) {
-                        setState(() {
-                          _progress = newValue;
-                        });
-                      },
-                    ),
-                  ),
-
-                  // 第二个右侧按钮
-                  _buildPlayback(),
                 ],
               ),
             ),
-
-            Expanded(
-              flex: 3, // 左侧按钮占20%
-              child: _buildPlaylist(),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class TopMenuBar extends StatelessWidget {
+  final VoidCallback? onPlaylistPressed;
+
+  const TopMenuBar({Key? key, this.onPlaylistPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue, // Placeholder color
+      child: Row(
+        children: [
+          PlaybackControls(),
+          VolumeControls(), // Add VolumeControls here
+          Spacer(),
+          IconButton(
+            icon: Icon(Icons.queue_music),
+            onPressed: onPlaylistPressed,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PlaybackControls extends StatelessWidget {
+  void _playPause() {
+    // Play/pause logic
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: Icon(Icons.shuffle),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.skip_previous),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.pause), // Assuming initial state is paused
+          onPressed: _playPause,
+        ),
+        IconButton(
+          icon: Icon(Icons.skip_next),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.repeat),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
+
+class VolumeControls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Slider(
+          value: 0.5, // Placeholder value, replace with _volume
+          onChanged: (double value) {
+            // Adjust volume based on value
+          },
+        ),
+      ],
     );
   }
 }
